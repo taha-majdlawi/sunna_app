94bb420d2070d62d41a0b925d937be3f557b88a2
TAHA MAJDLAWI <tahaat659@gmail.com>
2026-02-14 13:40:41 +0200

    Add Windows runner support with console output and DPI awareness
    - Introduced a manifest file for the Windows runner to enable DPI awareness and support for Windows 10 and 11.
    - Implemented utility functions for creating a console, converting command line arguments from UTF-16 to UTF-8, and handling console output redirection.
    - Developed the Win32Window class to manage high DPI-aware windows, including theme updates based on system preferences.
    - Added necessary header files and resource management for window creation and destruction.

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen());
+    return MaterialApp(home: HomeScreen(), debugShowCheckedModeBanner: false);
   }
 }
